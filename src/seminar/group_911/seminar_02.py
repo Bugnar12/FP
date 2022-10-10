37383dc196ca3c1fcc92e0f6271383565aabe502
Bugnar12 <113298199+Bugnar12@users.noreply.github.com>
2022-10-09 23:45:48 -0700

    Work...

--- src/seminar/group_911/seminar_02.py
+++ src/seminar/group_911/seminar_02.py
@@ -1,5 +1,41 @@
 """
 Let's write a menu-driven application. What does the menu look like?
-    1. Generate (random) person names
-    2. Exit
+    1.Generate (random) person names
+    2.Exit
 """
+import random
+
+def generate_names():
+    count = int(input("How many names to generate? "))
+    family_names = ["Popescu", "Albu", "Gheorghe", "Morar", "Negrea", "Bodnar"] #list()
+    given_names = ["Anca", "Elisa", "Ionut", "Vasile", "Iona", "Rares"]
+    result = []
+
+    for i in range(count):
+        family_name = random.randint(0, len(family_names) - 1)
+        given_name = random.randint(0, len(given_names) - 1)
+        name = family_names[family_name] + " " + given_names[given_name]
+        result.append(name)
+
+    #Let's print out the names
+    print(result)
+    return result
+
+def start():
+    print("welcome to seminar 2")
+
+    while True:
+        print("1.Generate (random) person name")
+        print("2.Exit")
+
+        opt = input(">")
+
+        if opt == "1":
+            generate_names()
+        elif opt == "2": #elif = else if
+            return   # break would have also been acceptable
+        else:
+            print("Bad command or file name")
+
+
+start()
