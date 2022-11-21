8338248ea026d3b5f38af03b11e0963534559d0a
Bugnar12 <113298199+Bugnar12@users.noreply.github.com>
2022-11-20 22:57:34 -0800

    Changes

---
+++ src/seminar/group_911/seminar_08/__init__.py
@@ -0,0 +1,18 @@
+"""
+Turning seminar 7 game ino object-oriented representation
+
+1.Let's turn tthe board module functions into the Board class
+    How can we protect class attributes from outside changes ?
+
+    protecting class attributes from outside change :
+    C#, Java, Kotlin, C++ -> public, private, protected
+        -public -> anyone can access and change
+        -private -> only class functions can access and change
+        -protected -> only class functions and derived classes can access and change
+    (default)
+
+    Python
+    <var.name> -> public
+    _<var.name> -> private
+    __<var.name> -> protected
+"""
